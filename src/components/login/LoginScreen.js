8750368302f0e9cb42b849d4d3662c7ca745ecc4
JCamiloo <jcamilo.osorio15@gmail.com>
2022-02-10 16:25:32 -0500

    session with useContext

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,9 +1,22 @@
+import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { types } from '../../types/types';
+import { AuthContext } from '../../auth/authContext';
 
 export const LoginScreen = () => {
   const navigate = useNavigate();
+  const { dispatch } = useContext(AuthContext);
 
   const handleLogin = () => {
+    const action = {
+      type: types.login,
+      payload: {
+        name: 'Juan',
+      }
+    }
+
+    dispatch(action);
+
     navigate('/marvel', {
       replace: true
     });
